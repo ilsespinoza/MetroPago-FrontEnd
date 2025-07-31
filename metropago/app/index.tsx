90bc1c2d4b357392b8513182df5bc83ad71514dd
ilsespinoza <104655979+ilsespinoza@users.noreply.github.com>
2025-07-31 14:22:32 -0700

    login pantalla inicio

--- metropago/app/index.tsx
+++ metropago/app/index.tsx
@@ -5,7 +5,7 @@ export default function Index() {
   const router = useRouter();
 
   useEffect(() => {
-    router.replace('./(tabs)/index.tsx'); 
+    router.replace('/login/login'); 
   }, []);
 
   return null; 
